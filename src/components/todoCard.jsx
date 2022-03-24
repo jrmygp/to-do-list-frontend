29bf826483fdfaa9f21b20927375b1f74ea15ef7
jrmygp <fat.snorlax17@gmail.com>
2022-03-24 19:20:51 +0700

    first commit to do list - front end

---
+++ src/components/todoCard.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { Box, Button, Flex, Input, Text } from "@chakra-ui/react";
+import { axiosInstance } from "../config/api";
+
+const TodoItem = ({ action, status, id }) => {
+  const [contentList, setContentList] = useState([]);
+  const [inputAction, setInputAction] = useState("");
+  const [inputStatus, setInputStatus] = useState("")
+  const fetchTodoList = () => {
+    axiosInstance.get("/todoLists").then((res) => {
+      setContentList(res.data.result);
+    });
+  };
+
+  const deleteList = () => {
+    axiosInstance.delete(`/todoLists/${id}`).then(() => {
+      fetchTodoList();
+    });
+  };
+  
+  const handleActionInput = (event) => {
+      const {value} = event.target
+      setInputAction(value)
+
+  }
+
+  const handleStatusInput = (event) => {
+      const {value} = event.target
+      setInputStatus(value)
+  }
+  const editList = () => {
+      const newList = {
+        action: inputAction,
+        status: inputStatus
+      }
+    axiosInstance.patch(`todoLists/${id}`, newList).then(() => {
+      fetchTodoList();
+    });
+  };
+
+  return (
+    <Box mb={5} border="1px solid gray" padding="5px" borderRadius={8} maxW="450px">
+      <Text mr={5}>Action: {action}</Text>
+      <Text mr={5} mb={2}>Status: {status}</Text>
+      <Input placeholder="Action" onChange={handleActionInput} mb={2}></Input>
+      <Input placeholder="Status" onChange={handleStatusInput} mb={2}></Input>
+      <Button colorScheme="green" mr={3} onClick={() => editList(id)}>
+        Edit
+      </Button>
+      <Button colorScheme="red" onClick={() => deleteList(id)}>
+        Delete
+      </Button>
+    </Box>
+  );
+};
+
+export default TodoItem;
---
+++ src/pages/todolist/index.jsx
@@ -0,0 +1,70 @@
+import {
+  Box,
+  Flex,
+  Heading,
+  Center,
+  Text,
+  Button,
+  Input,
+  Stack,
+} from "@chakra-ui/react";
+import { axiosInstance } from "../../config/api";
+import { useState, useEffect } from "react";
+import TodoItem from "../../components/todoCard";
+
+const todoList = () => {
+  const [contentList, setContentList] = useState([]);
+  const [newList, setNewList] = useState("");
+  const [newStatus, setNewStatus] = useState("");
+  const fetchTodoList = () => {
+    axiosInstance.get("/todoLists").then((res) => {
+      setContentList(res.data.result);
+    });
+  };
+  const renderContentList = () => {
+    return contentList.map((val) => {
+      return <TodoItem action={val?.action} id={val?.id} status={val.status} />;
+    });
+  };
+
+  const handleAction = (event) => {
+    const { value } = event.target;
+    setNewList(value);
+  };
+
+  const handleStatus = (event) => {
+    const { value } = event.target;
+    setNewStatus(value);
+  };
+  const addNewList = () => {
+    const newData = {
+      action: newList,
+      status: newStatus,
+    };
+    axiosInstance.post("/todoLists", newData).then(() => {
+      fetchTodoList();
+    });
+  };
+
+  useEffect(() => {
+    fetchTodoList();
+  }, []);
+  return (
+    <Center mt={6}>
+      <Stack>
+        <Box border="1px solid black" maxW="450px" p="5px" borderRadius={8}>
+            <Text mb={2} mr={4} fontWeight="bold" fontSize="lg">Add new list</Text>
+          <Input placeholder="Input new list" onChange={handleAction} mb={2}></Input>
+          <Input placeholder="Input new status" onChange={handleStatus} mb={2}></Input>
+
+          <Flex>
+            <Button colorScheme="blue" onClick={addNewList}>Add list</Button>
+          </Flex>
+        </Box>
+        {renderContentList()}
+      </Stack>
+    </Center>
+  );
+};
+
+export default todoList;
